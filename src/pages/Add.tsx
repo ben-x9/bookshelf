3e4310e1270659b44741b7fd208c593031715190
Benjamin James <ben.jx9@gmail.com>
2022-08-18 22:47:31 +0900

    Create the add book page from Figma design

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -2,12 +2,30 @@ import "./Add.css"
 
 export default function Add() {
   return (
-    <div>
+    <div className="Add">
       <header>
         <h1>Add a new book</h1>
         <img src="cross.svg" />
       </header>
-      <main></main>
+      <main>
+        <label>
+          Title
+          <input />
+        </label>
+        <label>
+          Author
+          <input />
+        </label>
+        <label>
+          Description
+          <textarea />
+        </label>
+        <label>
+          Image URL
+          <input />
+        </label>
+        <button onClick={() => {}}>Save</button>
+      </main>
     </div>
   )
 }
